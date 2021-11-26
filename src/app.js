49d26562e14db1dd6762942fc2467e8ced733ddd
Andre Korkin <andre_korkin@mail.ru>
2021-11-26 17:40:37 +0600

    add basic menu

--- src/app.js
+++ src/app.js
@@ -1,2 +1,11 @@
 import './styles.css'
+import {ContextMenu} from './menu'
+
+
+const $body = document.querySelector('body')
+const $context_menu = new ContextMenu('#menu')
+$body.addEventListener('contextmenu', (event) => {
+    event.preventDefault()
+    $context_menu.open(event.clientX, event.clientY)
+})
 
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,14 @@
 import {Menu} from './core/menu'
 
-export class ContextMenu extends Menu {
 
+export class ContextMenu extends Menu {
+    constructor(selector) {
+        super(selector)
+    }
+
+    open(x, y) {
+        this.el.style.top = y + 'px'
+        this.el.style.left = x + 'px'
+        this.el.classList.add('open')
+    }
 }
