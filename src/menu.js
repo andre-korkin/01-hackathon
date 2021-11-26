47f9ef95e9a838a8476961edc884610d87a91d97
Andre Korkin <andre_korkin@mail.ru>
2021-11-26 23:37:26 +0600

    menu whit items

--- src/menu.js
+++ src/menu.js
@@ -4,11 +4,27 @@ import {Menu} from './core/menu'
 export class ContextMenu extends Menu {
     constructor(selector) {
         super(selector)
+        this.items = []
     }
 
     open(x, y) {
         this.el.style.top = y + 'px'
         this.el.style.left = x + 'px'
         this.el.classList.add('open')
+    }
+
+    close() {
+        this.el.classList.remove('open')
+    }
+
+    add(arr) {
+        arr.forEach(item => {
+            this.items.push(item)
+            this.el.innerHTML += item.toHTML()
+            this.el.querySelector(`[data-type="${item.type}"]`).addEventListener('click', () => {
+                this.close()
+                item.trigger()
+            })
+        })
     }
 }
