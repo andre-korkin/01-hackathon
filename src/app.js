bb678a8b41067c913d024201faa58936cfbb4729
Andre Korkin <andre_korkin@mail.ru>
2021-11-27 09:57:19 +0600

    right coordinates of menu and comments in menu.js

--- src/app.js
+++ src/app.js
@@ -7,5 +7,5 @@ const context_menu = new ContextMenu('#menu')
 const $body = document.querySelector('body')
 $body.addEventListener('contextmenu', (event) => {
     event.preventDefault()
-    context_menu.items.length > 0 ? context_menu.open(event.clientX, event.clientY) : false
+    context_menu.items.length > 0 ? context_menu.open(event) : false  // отрисовываем меню только тогда, когда массив пунктов меню не пустой
 })
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,6 @@
 import {Menu} from './core/menu'
 
+// -------------- импортируем модули списков меню ---------
 import {SettingsModule} from './modules/settings.module'
 import {ShapeModule} from './modules/shape.module'
 import {ClicksModule} from './modules/clicks.module'
@@ -9,34 +10,44 @@ import {BackgroundModule} from './modules/background.module'
 export class ContextMenu extends Menu {
     constructor(selector) {
         super(selector)
-        this.items = [
+        this.items = [  // создаем массив пунктов меню
             new SettingsModule('settings', 'Настройка меню'),
             new ShapeModule('shape', 'Случайная фигура'),
             new ClicksModule('clicks', 'Подсчет кликов'),
             new BackgroundModule('background', 'Случайный фон')
         ]
 
-        this.add()
-        this.clicker()
+        this.add()  // отрисовка пунктов меню
+        this.clicker()  // навешивание кликов на пункты меню
     }
 
-    open(x, y) {
+    open(event) {  // функция отрисовки меню
+        const win_width = document.documentElement.clientWidth  // ширина документа
+        const win_height = document.documentElement.clientHeight  // высота документа
+        const menu_width = 200  // ширина меню
+        const menu_height = 30*this.items.length  // высота меню = общей высоте пунктов меню
+        let x = event.clientX  // X-координата курсора
+        let y = event.clientY  // Y-координата курсора
+
+        y + menu_height + 20 > win_height ? y -= menu_height : false  // отработка правильного расположения меню по горизонтали
+        x + menu_width + 20 > win_width ? x -= menu_width : false  // и по вертикали (чтобы не уходило за край экрана)
+
         this.el.style.top = y + 'px'
         this.el.style.left = x + 'px'
         this.el.classList.add('open')
     }
 
-    close() {
+    close() {  // функция скрытия меню
         this.el.classList.remove('open')
     }
 
-    add() {
-        this.items.forEach(item => {
+    add() {  // функция отрисовки пунктов меню (перебираем массив пунктов)
+        this.items.forEach(item => {  // 
             this.el.innerHTML += item.toHTML()
         })
     }
 
-    clicker() {
+    clicker() {  // функция навешивания кликов на пункты меню (перебираем массив пунктов)
         this.items.forEach(item => {
             this.el.querySelector(`[data-type="${item.type}"]`).addEventListener('click', () => {
                 this.close()
